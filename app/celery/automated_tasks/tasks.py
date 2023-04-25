2966c81874c4ee5cdc0fe9e830273a9558576e05
francois.ancien <francois.ancien@uni.lu>
2023-04-25 10:43:42 -0700

    Adding celery task docstring

--- app/celery/automated_tasks/tasks.py
+++ app/celery/automated_tasks/tasks.py
@@ -5,9 +5,29 @@ from app.celery.celery_app import app
 
 from ... import routers, models
 
-
 @app.task
-def f1_model_persistent_identifier(task_dict, data):
+def f1_model_persistent_identifier(task_dict: dict, data: dict) -> None:
+    """
+    Representation of celery task to evaluate an assessment.
+    These celery tasks should be in the format:
+    ```
+    def assessment_task(task_dict: dict, data: dict) -> None:
+        session_id = task_dict["session_id"]
+        task_id = task_dict["id"]
+
+        # Code to get the final TaskStatus
+        result = evaluate_assessment(data)
+
+        status = models.TaskStatusIn(status=models.TaskStatus(result))
+        requests.patch(
+            f"http://localhost:8000/session/{session_id}/tasks/{task_id},
+            json=status
+        )
+
+    :param task_dict: Task dict representation
+    :param data: (Meta)Data to evaluate
+    :return: None
+    """
     print("Execution successfully called")
     sleep(10)
     session_id = task_dict["session_id"]
@@ -25,4 +45,3 @@ def f1_model_persistent_identifier(task_dict, data):
     requests.patch(f"http://localhost:8000/session/{session_id}/tasks/{task_id}", json=status.dict())
     # routers.update_task(session_id, task_id, status)  # Does not work because celery does not have access to fair_indicators
     # redis_app.json().set(f"session:{session_id}", f".tasks.{task_id}.status", obj=result)  # Works, but does not trigger udpdating of children
-    return result
