7820d3151b51e19a577bfba76b70863c628df743
francois.ancien <francois.ancien@uni.lu>
2023-04-25 15:37:47 -0700

    Removing unnecessary comment

--- app/celery/automated_tasks/tasks.py
+++ app/celery/automated_tasks/tasks.py
@@ -41,7 +41,7 @@ def f1_model_persistent_identifier(task_dict: dict, data: dict) -> None:
     status = models.TaskStatusIn(status=models.TaskStatus(result))
 
     print(f"Task status computed: {result}")
-    # Does not work because Task is disabled (of course, it's automatic)
     requests.patch(f"http://localhost:8000/session/{session_id}/tasks/{task_id}", json=status.dict())
+
     # routers.update_task(session_id, task_id, status)  # Does not work because celery does not have access to fair_indicators
-    # redis_app.json().set(f"session:{session_id}", f".tasks.{task_id}.status", obj=result)  # Works, but does not trigger udpdating of children
+    # redis_app.json().set(f"session:{session_id}", f".tasks.{task_id}.status", obj=result)  # Works, but does not trigger updating of children
