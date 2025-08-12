de31a3d87d6a85f0d3a57b0de5b28dcc87dbc489
Tom Gebhardt <tom.gebhardt@uni-greifswald.de>
2025-08-12 16:04:25 +0200

    WIP

---
+++ app/celery/automated_tasks/hsh_helpers.py
@@ -0,0 +1,27 @@
+import json
+
+def check_route(metadata, route_keys):
+    current_position = json.loads(metadata)
+    for key in route_keys:
+        if key in current_position:
+            current_position = current_position[key]
+        else:
+            print("the key {} form the route{} is not in the dict".format(key, route_keys), )
+            #if a key is missing return false
+            return False
+    #if the route exists return the value
+    return current_position
+
+def check_list(data, checks):
+    for attr in checks:
+        if not check_route(data, attr): return False
+
+    return True
+
+def is_url_reachable(url):
+            try:
+                response = requests.head(url, timeout=5)  # Send a HEAD request to check if the server is reachable
+                return response.status_code < 400  # If the status code is less than 400, the URL is reachable
+            except requests.RequestException:
+                return False  # If any exception occurs (e.g., timeout), consider the URL unreachable
+
