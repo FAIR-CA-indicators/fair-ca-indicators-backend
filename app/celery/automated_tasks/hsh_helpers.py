d6c111cfe5b110ee82ed53405ef5643e8798a7a1
Tom Gebhardt <tom.gebhardt@uni-greifswald.de>
2026-03-05 13:23:55 +0100

    WIP: Debugging the indicators

--- app/celery/automated_tasks/hsh_helpers.py
+++ app/celery/automated_tasks/hsh_helpers.py
@@ -15,13 +15,14 @@ def check_route(metadata, route_keys):
 
 def check_list(data, checks):
     for attr in checks:
-        if not check_route(data, attr): return False
+        if not check_route(data, attr):
+            print("check failed for: ", attr)
+            return False
     return True
 
 def is_url_reachable(url):
     try:
         response = requests.head(url, timeout=5)  # Send a HEAD request to check if the server is reachable
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ", response.status_code)
         return response.status_code < 400  # If the status code is less than 400, the URL is reachable
     except requests.RequestException:
         return False  # If any exception occurs (e.g., timeout), consider the URL unreachable
